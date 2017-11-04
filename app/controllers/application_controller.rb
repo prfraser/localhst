a4142aa78b643da4ec5e4ea3918d9013ef3faf19
philip fraser <pr.fraser3@gmail.com>
2017-11-05 09:56:15 +1000

    changed admin app controller setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-    def authenticate_admin_user!
-	    redirect_to new_user_session_path
-	    puts "asdfasdf"
-	    if current_user && current_user.is_admin
-	      # fine
-	    else
-	      redirect_to new_user_session_path
-	    end
-	  end
 end
