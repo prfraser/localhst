034f579eef9d3d1ffbacd77d19fd1ef338c36ff3
philip fraser <pr.fraser3@gmail.com>
2017-11-05 09:29:57 +1000

    added logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+    def authenticate_user!
+	    redirect_to new_user_session_path
+	    puts "asdfasdf"
+	    if current_user && current_user.is_admin
+	      # fine
+	    else
+	      redirect_to new_user_session_path
+	    end
+	  end
 end
