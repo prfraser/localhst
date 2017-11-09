5451ff822f0f8e7ddc9dd2d2beac550e8df10629
philip fraser <pr.fraser3@gmail.com>
2017-11-09 10:39:50 +1000

    changed mailer settings

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -1,4 +1,5 @@
 class SignupMailer < ApplicationMailer
+	default :from => 'localhst.tour@gmail.com'
 	def signup_confirmation(to_email)
 		@to_email = to_email
 		mail(to: @to_email, subject: "Localhst Signup")
