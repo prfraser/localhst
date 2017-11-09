1b41b33485e9ecbedf1db65efa010f5f1260dc93
philip fraser <pr.fraser3@gmail.com>
2017-11-10 08:12:02 +1000

    fixed profile show on signed out users bug

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,8 @@ class ProfileController < ApplicationController
   def show
   	@user = User.find(params[:user])
   	@tours = @user.tours
-  	@stripe_user_id = current_user.stripe_user_id
+  	if current_user
+	  	@stripe_user_id = current_user.stripe_user_id
+	  end
   end
 end
